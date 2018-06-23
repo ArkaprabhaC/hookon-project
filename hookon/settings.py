64449d9ad658f25065c8cfff536e5d4196d3e2c6
chayan007 <sonicxxx7@gmail.com>
2018-06-23 10:29:30 -0700

    Student Models created

--- hookon/settings.py
+++ hookon/settings.py
@@ -118,4 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'hookon/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
