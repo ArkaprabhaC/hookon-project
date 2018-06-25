5c0794980c2eeeeb3b940e3d65d7bb63fdb7f80f
chayan007 <sonicxxx7@gmail.com>
2018-06-24 23:01:25 -0700

    Admin Panel 85%

--- hookon/settings.py
+++ hookon/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
--- internship/admin.py
+++ internship/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Student, Interns
+from .models import Student, Interns,Recruiter,Internships
 # Register your models here.
 
 admin.site.register(Student)
 admin.site.register(Interns)
+admin.site.register(Recruiter)
+admin.site.register(Internships)
